66c12ae2eefe5f389c55fe7a76d938110db63067
Aleš <aleswita@gmail.com>
2020-03-23 18:08:12 +0100

    phpstan

--- src/LocaleResolver.php
+++ src/LocaleResolver.php
@@ -45,7 +45,7 @@ class LocaleResolver
 			$resolver = $this->container->getByType($v1);
 			$locale = $resolver->resolve($translator);
 
-			if ($locale !== null && in_array(Nette\Utils\Strings::substring($locale, 0, 2), array_map(function ($locale): string {
+			if ($locale !== null && $translator->getLocalesWhitelist() === null || in_array(Nette\Utils\Strings::substring($locale, 0, 2), array_map(function ($locale): string {
 				return Nette\Utils\Strings::substring($locale, 0, 2);
 			}, $translator->getLocalesWhitelist()), true)) {
 				return $locale;
