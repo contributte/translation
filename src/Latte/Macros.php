5f0fe12c19364ee78c6fb2bfcbcae34f170f1e50
Aleš <aleswita@gmail.com>
2020-11-17 21:03:48 +0100

    fix for lower version of latte

--- src/Latte/Macros.php
+++ src/Latte/Macros.php
@@ -22,13 +22,11 @@ class Macros extends Latte\Macros\MacroSet
 	}
 
 	/**
-	 * https://github.com/nette/latte/blob/master/src/Latte/Macros/CoreMacros.php#L205
-	 *
 	 * {_ ...}
 	 *
 	 * @throws Latte\CompileException
 	 */
-	public function macroTranslate(Latte\MacroNode $node, Latte\PhpWriter $writer): ?string
+	public function macroTranslate(Latte\MacroNode $node, Latte\PhpWriter $writer): string
 	{
 		if ($node->closing) {
 			if (strpos($node->content, '<?php') === false) {
@@ -40,12 +38,13 @@ class Macros extends Latte\Macros\MacroSet
 				$value = 'ob_get_clean()';
 			}
 
+			if (Latte\Engine::VERSION_ID < 20900) {
+				return $writer->write('$_fi = new LR\FilterInfo(%var); echo %modifyContent($this->filters->filterContent("translate", $_fi, %raw))', $node->context[0], $value);
+			}
+
 			return $writer->write('$__fi = new LR\FilterInfo(%var); echo %modifyContent($this->filters->filterContent("translate", $__fi, %raw))', $node->context[0], $value);
-		}
 
-		if ($node->empty = ($node->args !== '')) {
-			// return $writer->write('echo %modify(($this->filters->translate)(%node.args))');
-
+		} elseif ($node->empty = ($node->args !== '')) {
 			if (Contributte\Translation\Helpers::macroWithoutParameters($node)) {
 				return $writer->write('echo %modify(call_user_func($this->filters->translate, %node.word))');
 			}
@@ -53,7 +52,7 @@ class Macros extends Latte\Macros\MacroSet
 			return $writer->write('echo %modify(call_user_func($this->filters->translate, %node.word, %node.args))');
 		}
 
-		return null;
+		return '';
 	}
 
 	/**
@@ -61,14 +60,14 @@ class Macros extends Latte\Macros\MacroSet
 	 *
 	 * @throws Latte\CompileException
 	 */
-	public function macroPrefix(Latte\MacroNode $node, Latte\PhpWriter $writer): ?string
+	public function macroPrefix(Latte\MacroNode $node, Latte\PhpWriter $writer): string
 	{
 		if ($node->closing) {
 			if ($node->content !== null && $node->content !== '') {
 				return $writer->write('$this->global->translator->prefix = $this->global->translator->prefixTemp;');
 			}
 
-			return null;
+			return '';
 		}
 
 		if ($node->args === '') {
