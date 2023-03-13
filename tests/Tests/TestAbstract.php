a8192980a8b48df411c3be68a22a082b75e41505
Pavel Železný <pavel.zelezny@interitty.org>
2023-03-13 20:07:37 +0100

    Add support for nette\utils:4.0.0 (#115) (#116)

--- tests/Tests/TestAbstract.php
+++ tests/Tests/TestAbstract.php
@@ -10,11 +10,29 @@ abstract class TestAbstract extends TestCase
 
 	protected Container $container;
 
+	protected bool $isNewNetteUtils;
+
 	public function __construct(
 		Container $container
 	)
 	{
+		if (class_exists('\Composer\InstalledVersions')) { // Composer 2
+			$netteUtilsVersion = \Composer\InstalledVersions::getPrettyVersion('nette/utils');
+		} else { // Composer 1
+			$composerRaw = \Nette\Utils\FileSystem::read(__DIR__ . '/../../composer.lock');
+			$composerData = \Nette\Utils\Json::decode($composerRaw);
+			$netteUtilsVersion = '0.0.0';
+			foreach ($composerData->packages as $package) {
+				if ($package->name !== 'nette/utils') {
+					continue;
+				}
+
+				$netteUtilsVersion = ltrim($package->version, 'v');
+			}
+		}
+
 		$this->container = $container;
+		$this->isNewNetteUtils = version_compare($netteUtilsVersion, '4.0.0', '>=');
 	}
 
 }
